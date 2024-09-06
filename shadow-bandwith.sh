cd310d262e9d9c4427dd4962739189765d1e63f0
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-09-06 16:24:04 +0330

    Update shadow-bandwith.sh

--- shadow-bandwith.sh
+++ shadow-bandwith.sh
@@ -14,10 +14,10 @@ tx_mb=$((tx_bytes / 1048576))
 if ((rx_mb < 1024)); then
     rx_unit="MB"
     rx_value=$rx_mb
-elif ((rx_mb >= 1024)); then
+elif (((1024 * 1024) < rx_mb >= 1024)); then
     rx_unit="GB"
     rx_value=$(printf "%.2f" "$(bc -l <<< "$rx_mb / 1024")")
-elif ((rx_mb >= 1024 * 1024)); then
+elif ((rx_mb >= (1024 * 1024))); then
     rx_unit="TB"
     rx_value=$(printf "%.2f" "$(bc -l <<< "$rx_mb / (1024 * 1024)")")
 
@@ -25,10 +25,10 @@ fi
 if ((tx_mb < 1024)); then
     tx_unit="MB"
     tx_value=$rx_mb
-elif ((tx_mb >= 1024)); then
+elif (((1024 * 1024) < tx_mb >= 1024)); then
     tx_unit="GB"
     tx_value=$(printf "%.2f" "$(bc -l <<< "$tx_mb / 1024")")
-elif ((rx_mb >= 1024 * 1024)); then
+elif ((tx_mb >= (1024 * 1024))); then
     tx_unit="TB"
     tx_value=$(printf "%.2f" "$(bc -l <<< "$tx_mb / (1024 * 1024)")")
 
